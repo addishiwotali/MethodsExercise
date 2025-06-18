1c0536770fbd9d13093a187b87f6f3938ca1ed16
Addishiwot Ali <sosenamillion@yahoo.com>
2025-06-17 22:53:55 -0700

    completed Methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,57 @@
 {
     public class Program
     {
+        public static void Madlib()
+        {
+            Console.WriteLine("what's your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("how old are you? ");
+
+            string old = Console.ReadLine();
+
+            Console.WriteLine("what's your favorite day?");
+            
+            string day = Console.ReadLine();
+
+            Console.WriteLine("what's youe favorite food?");
+            
+            string favorite = Console.ReadLine();
+
+            Console.WriteLine($"a women named {name} was celebrating her {old} birthday on {day} eating {favorite}");
+            
+
+
+        }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo; 
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Madlib();
+           // Console.WriteLine(Add(1, 5));
+           // Console.WriteLine(Subtract(3, 2));
+           //Console.WriteLine(Multiply(2, 2));
+           //Console.WriteLine(Divide(5, 1));
         }
-    }
+    } 
 }
